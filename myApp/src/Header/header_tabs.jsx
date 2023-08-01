8b57ccf458798344673daabadb17ededffdacc2e
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-01 19:58:29 +0800

    Update deleted/reconstructed files

--- myApp/src/Header/header_tabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function HeaderTabs () {
-    return (
-        <>
-        <button className="home">Home</button>
-        <button className="collection">Collection</button>
-        <button className="aboutUs">About Us</button>
-        </>
-    )
-}
