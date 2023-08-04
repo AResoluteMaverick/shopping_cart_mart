d91789007089e2cc63694654863a37afa2a2c15f
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-04 21:29:24 +0800

    Stylize Footer component links, and add a division

--- ShoppingCart-and-Mart/src/Footer/Footer.jsx
+++ ShoppingCart-and-Mart/src/Footer/Footer.jsx
@@ -5,41 +5,41 @@ function FooterLinks () {
     return (
         <div className={styles.linkContainer}>
             <div>
-                <div className = {styles.dummyTitle}>This is a Title</div>
-                <ul className = {styles.dummyLinks}>
-                    <li><Link to="/error">This is a link</Link></li>
-                    <li><Link to="/error">This is a Link</Link></li>
-                    <li><Link to="/error">This is a Link</Link></li>
-                    <li><Link to="/error">This is a LInk</Link></li>
+                <div className={styles.dummyTitle}>COMPANY</div>
+                <ul className={styles.dummyLinks}>
+                    <li><Link to="/error">About Us</Link></li>
+                    <li><Link to="/error">Our Team</Link></li>
+                    <li><Link to="/error">Careers</Link></li>
+                    <li><Link to="/error">Contact</Link></li>
                 </ul>
             </div>
 
             <div>
-                <div className = {styles.dummyTitle}>This is a Title</div>
-                <ul className = {styles.dummyLinks}>
-                    <li><Link to="/error">This is a link</Link></li>
-                    <li><Link to="/error">This is a Link</Link></li>
-                    <li><Link to="/error">This is a Link</Link></li>
-                    <li><Link to="/error">This is a LInk</Link></li>
+                <div className={styles.dummyTitle}>RESOURCES</div>
+                <ul className={styles.dummyLinks}>
+                    <li><Link to="/error">Blog</Link></li>
+                    <li><Link to="/error">News</Link></li>
+                    <li><Link to="/error">Support</Link></li>
+                    <li><Link to="/error">FAQs</Link></li>
                 </ul>
             </div>
 
             <div>
-                <div className = {styles.dummyTitle}>This is a Title</div>
-                <ul className = {styles.dummyLinks}>
-                    <li><Link to="/error">This is a link</Link></li>
-                    <li><Link to="/error">This is a Link</Link></li>
-                    <li><Link to="/error">This is a Link</Link></li>
-                    <li><Link to="/error">This is a LInk</Link></li>
+                <div className={styles.dummyTitle}>SHOP</div>
+                <ul className={styles.dummyLinks}>
+                    <li><Link to="/error">Products</Link></li>
+                    <li><Link to="/error">Categories</Link></li>
+                    <li><Link to="/error">Cart</Link></li>
+                    <li><Link to="/error">Account</Link></li>
                 </ul>
             </div>
-    </div>
+        </div>
     )
 }
 
 function SocialIcons () {
     return (
-    <div className={styles.socialLinks}>
+        <div className={styles.socialLinks}>
         <div className="twitter socials">T</div>
         <div className="facebook socials">F</div>
         <div className="youtube socials">Y</div>
