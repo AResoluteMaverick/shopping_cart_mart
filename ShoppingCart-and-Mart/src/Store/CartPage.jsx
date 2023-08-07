321fcae0a0aa100b193994f7701d7e12fb15bb41
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-07 18:27:06 +0800

    Create quantity logic to cart
    1. Update cartSlice and CartPage

--- ShoppingCart-and-Mart/src/Store/CartPage.jsx
+++ ShoppingCart-and-Mart/src/Store/CartPage.jsx
@@ -1,13 +1,11 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './CartPage.module.css';
-import { removeFromCart } from './cartSlice';
-import { useDispatch } from 'react-redux';
-
+import { removeFromCart, updateQuantity } from './cartSlice';
 
 function getTotal(cartItems) {
-  return cartItems.reduce((total, item) => total + item.price, 0).toFixed(2);
+  return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
 }
 
 export default function CartPage() {
@@ -25,11 +23,16 @@ export default function CartPage() {
           <>
           <div className={styles.cartItemsContainer}>
            {cartItems.map((item, index) => (
-             <div key={index} className={styles.cartItem}>
-             <h3>{item.title}</h3>
-             <p>Price: {item.price}</p>
-             <button onClick={() => dispatch(removeFromCart(index))}>Delete Item</button>
-           </div>
+              <div key={index} className={styles.cartItem}>
+                  <h3>{item.title}</h3>
+                  <p>Price: {item.price}</p>
+                  <p>Quantity: {item.quantity}</p>
+                  <button onClick={() => dispatch(removeFromCart(index))}>Delete Item</button>
+                  <button onClick={() => dispatch(updateQuantity({ index, quantity: item.quantity + 1 }))}>Increase Quantity</button>
+                  <button onClick={() => dispatch(updateQuantity({ index, quantity: item.quantity - 1 }))} disabled={item.quantity <= 1}>
+                    Decrease Quantity
+                  </button>
+              </div>
           ))}
           </div>
             <div className={styles.itemCalculationContainer}>
--- ShoppingCart-and-Mart/src/Store/cartSlice.js
+++ ShoppingCart-and-Mart/src/Store/cartSlice.js
@@ -5,15 +5,27 @@ export const cartSlice = createSlice({
   initialState: [],
   reducers: {
     addToCart: (state, action) => {
-      state.push(action.payload)
+      const item = action.payload;
+      const existingItem = state.find((i) => i.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.push({ ...item, quantity: 1 });
+      }
     },
     removeFromCart: (state, action) => {
       const index = action.payload;
       state.splice(index, 1);
     },
+    updateQuantity: (state, action) => {
+      const { index, quantity } = action.payload;
+      if (quantity >= 1) {
+        state[index].quantity = quantity;
+      }
+    },
   },
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions
 
 export default cartSlice.reducer
