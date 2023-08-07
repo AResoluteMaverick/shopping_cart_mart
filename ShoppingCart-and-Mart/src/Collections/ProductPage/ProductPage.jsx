9bb05b74b67201a2708acec7507286c146fc5b6e
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-07 14:31:08 +0800

    Install redux and it's react bindings

--- ShoppingCart-and-Mart/src/Collections/ProductPage/ProductPage.jsx
+++ ShoppingCart-and-Mart/src/Collections/ProductPage/ProductPage.jsx
@@ -7,6 +7,11 @@ import { useLocation } from 'react-router-dom'
 export default function ProductPage () {
     const location = useLocation()
     const { id, title, price, image, description } = location.state
+    const productItem = {id, price, title, image, description};
+
+    const handleAddToCart = (productItem) => {
+
+    };
 
     return(
         <>
@@ -21,7 +26,7 @@ export default function ProductPage () {
                 <div className={styles.addToCartWrapper}>
                     <div className={styles.productPrice}>{price}</div>
                     <div>input</div>
-                    <button>Add to Cart</button>
+                    <button onClick={() => handleAddToCart(productItem)}>Add to Cart</button>
                 </div>
             </div>
         </div>
