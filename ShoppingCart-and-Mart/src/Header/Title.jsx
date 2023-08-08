8c9406f8c77520423a27b172e81529cf002c8f0d
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-08 18:41:19 +0800

    Add styling to footer and Header
    - Additionally create a navbar animation

--- ShoppingCart-and-Mart/src/Header/Title.jsx
+++ ShoppingCart-and-Mart/src/Header/Title.jsx
@@ -1,12 +1,15 @@
 import styles from './Header.module.css'
 import { Link } from "react-router-dom";
+import carticon from '../assets/icons/shopping-bag-svgrepo-com.svg';
 
 
 export default function Title () {
     return (
         <div className={styles.titleDiv}>
-            <h1 className={styles.LogoTitle}>Lux</h1>
-            <Link to="/cart">Cart</Link>
+            <Link to="/">
+                <h1 className={styles.LogoTitle}>Lux</h1>
+            </Link>
+            <Link to="/cart"> <img src={carticon} alt="shoppingcart" className={styles.cartIcon}/> </Link>
         </div>
     )
 }
