6c4b4bf2d7671d80b1168d6b21ee8a7c0370e5ec
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-09 18:05:56 +0800

    Add cart styling

--- ShoppingCart-and-Mart/src/Store/CartPage.jsx
+++ ShoppingCart-and-Mart/src/Store/CartPage.jsx
@@ -3,6 +3,7 @@ import Footer from "../Footer/Footer";
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './CartPage.module.css';
 import { removeFromCart, updateQuantity } from './cartSlice';
+import checkoutIcon from '../assets/icons/shopping-cart-21-svgrepo-com.svg';
 
 function getTotal(cartItems) {
   return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
@@ -28,21 +29,23 @@ export default function CartPage() {
                     <img src={item.image} alt={item.title} className={styles.imagesrc}/>
                   </div>
                   <h3 className={styles.itemTitle}>{item.title}</h3>
-                  <p className={styles.itemPrice}>{item.price}</p>
+                  <p className={styles.itemPrice}>${item.price}</p>
                   <div className={styles.quantityWrapper}>
-                      <button onClick={() => dispatch(updateQuantity({ index, quantity: item.quantity + 1 }))}>-</button>
+                      <button className={styles.buttonQuantity} onClick={() => dispatch(updateQuantity({ index, quantity: item.quantity + 1 }))}>+</button>
                       <p className={styles.itemQuantity}>{item.quantity}</p>
-                      <button onClick={() => dispatch(updateQuantity({ index, quantity: item.quantity - 1 }))} disabled={item.quantity <= 1}>
-                        +
+                      <button className={styles.buttonQuantity} onClick={() => dispatch(updateQuantity({ index, quantity: item.quantity - 1 }))} disabled={item.quantity <= 1}>
+                        -
                       </button>
                   </div>
-                  <button onClick={() => dispatch(removeFromCart(index))}>x</button>
+                  <button className={styles.deleteButton} onClick={() => dispatch(removeFromCart(index))}>x</button>
               </div>
           ))}
           </div>
           <div className={styles.itemCalculationContainer}>
               <h2 className={styles.orderTotal}>Order Total: ${orderTotal}</h2>
-              <button className={styles.checkOut}>Check Out</button>
+              <button className={styles.checkOut}>Check Out
+              <img src={checkoutIcon} alt ="Checkout" className={styles.checkoutIcon} />
+              </button>
           </div>
           </>
         )}
